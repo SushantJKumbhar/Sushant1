c6a779e95d479ebe06c6914f4559ddbe6a4fd4d6
SushantJKumbhar <107764870+SushantJKumbhar@users.noreply.github.com>
2022-08-26 11:46:15 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "5. Please no visit my website learning-ocean.com";
+        return "5. Kindly visit my website learning-ocean.com";
     }
 
 }
